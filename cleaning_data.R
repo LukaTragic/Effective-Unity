0df70cfc03cf9de7e9f7fe684887a48a8729d607
Luka Taglic <lukataglic02@gmail.com>
2025-06-30 20:11:01 -0400

    Fixed ingestion

--- cleaning_data.R
+++ cleaning_data.R
@@ -1,18 +1,19 @@
 library(tidyverse)
-
+library(readxl)
 
 voteview_data <- read.csv("data/VV_H111-118_DW.csv")
-cel_data <- read.csv("data/CEL_H111-118_LES.csv")
+cel_data <- read_excel("data/CEL_H111-118_LES.xlsx")
 
 territories = c("AS", "DC", "GU", "MP", "PR", "VI")
 house_ideology <- voteview_data %>% filter(chamber == 'House', !state_abbrev %in% territories)
-house_effectiveness <- cel_data %>% filter(!Two.letter.state.code %in% territories)
+house_effectiveness <- cel_data %>% filter(!`Two-letter state code` %in% territories)
 
+colnames(house_effectiveness)
 
 house_effectiveness <- house_effectiveness %>%
   rename(
-    icpsr = `ICPSR.number..according.to.Poole.and.Rosenthal`,
-    congress = `Congress.number`
+    icpsr = `ICPSR number, according to Poole and Rosenthal`,
+    congress = `Congress number`
   )
 
 merged_house <- inner_join(
@@ -24,12 +25,12 @@ merged_house <- inner_join(
 colnames(merged_house)
 features <- c("congress", "chamber", "icpsr", "district_code", "state_abbrev", "party_code",
               "bioname", "bioguide_id", "nominate_dim1", "nominate_dim2", 
-              "nokken_poole_dim1", "nokken_poole_dim2", "Year.first.elected.to.House",
-              "Percent.vote.received.to.enter.this.Congress", "Size.of.House.delegation.from.member.s.state",
-              "X1...majority.party.member", "Seniority..number.of.terms.served.counting.current",
-              "Number.of.members.in.Party..0...Indep.", "LES.Classic.Rank.within.Party",
-              "LES.1.0", "Benchmark.score..Classic..based.on.majority..seniority..chairs", "LES.benchmark..Classic.",
-              "X1...Below..2...Meets..3...Above..Based.on.Classic.LES")
+              "nokken_poole_dim1", "nokken_poole_dim2", "Year first elected to House",
+              "Percent vote received to enter this Congress", "Size of House delegation from member's state",
+              "1 = majority party member", "Seniority, number of terms served counting current",
+              "Number of members in Party (0 = Indep)", "LES Classic Rank within Party",
+              "LES 1.0", "Benchmark score (Classic) based on majority, seniority, chairs", "LES/benchmark (Classic)",
+              "1 = Below, 2 = Meets, 3 = Above, Based on Classic LES")
 
 
 filtered_houses <- select(merged_house, features)
@@ -43,17 +44,17 @@ filtered_houses <- filtered_houses %>%
     party = `party_code`,
     DW_nominate_dim1 = `nominate_dim1`,
     DW_nominate_dim2 = `nominate_dim2`,
-    year_first_elected = `Year.first.elected.to.House`,
-    victory_margin = `Percent.vote.received.to.enter.this.Congress`,
-    size_state_delegation = `Size.of.House.delegation.from.member.s.state`,
-    majority_party_member = `X1...majority.party.member`,
-    num_terms_served = `Seniority..number.of.terms.served.counting.current`,
-    num_party_members = `Number.of.members.in.Party..0...Indep.`,
-    LES_rank_within_party = `LES.Classic.Rank.within.Party`,
-    LES_1.0 = `LES.1.0`,
-    benchmark_score = `Benchmark.score..Classic..based.on.majority..seniority..chairs`,
-    score = `LES.benchmark..Classic.`,
-    score_rank = `X1...Below..2...Meets..3...Above..Based.on.Classic.LES`
+    year_first_elected = `Year first elected to House`,
+    victory_margin = `Percent vote received to enter this Congress`,
+    size_state_delegation = `Size of House delegation from member's state`,
+    majority_party_member = `1 = majority party member`,
+    num_terms_served = `Seniority, number of terms served counting current`,
+    num_party_members = `Number of members in Party (0 = Indep)`,
+    LES_rank_within_party = `LES Classic Rank within Party`,
+    LES_1.0 = `LES 1.0`,
+    benchmark_score = `Benchmark score (Classic) based on majority, seniority, chairs`,
+    score = `LES/benchmark (Classic)`,
+    score_rank = `1 = Below, 2 = Meets, 3 = Above, Based on Classic LES`
   )
 
 
@@ -73,20 +74,5 @@ filtered_houses <- filtered_houses %>%
     LES_1.0, LES_rank_within_party, benchmark_score, score, score_rank
   )
 
+saveRDS(filtered_houses, "data/filtered_houses.rds")
 
-majority_by_congress <- filtered_houses %>%
-  group_by(congress, party) %>%
-  summarize(n = n(), .groups = "drop") %>%
-  group_by(congress) %>%
-  slice_max(order_by = n, n = 1, with_ties = FALSE) %>%
-  mutate(
-    house_majority_party = case_when(
-      party == 100 ~ "Democratic",
-      party == 200 ~ "Republican",
-      TRUE ~ "Other"
-    )
-  ) %>%
-  select(congress, house_majority_party)
-
-merged_house <- filtered_houses %>%
-  left_join(majority_by_congress, by = "congress")
